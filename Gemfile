36e4edaf312f396303829f713c50df5f63479ffd
Takashi Moriyama <ps034056@gmail.com>
2013-09-08 17:56:14 +0900

    画像を作成
    Signed-off-by: Takashi Moriyama <ps034056@gmail.com>

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.0'
 
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'sqlite3', '>= 1.3.8'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
@@ -36,6 +36,8 @@ group :assets do
   gem 'haml-rails'
   gem 'eco'
 end
+
+gem 'powder'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130824182950) do
+ActiveRecord::Schema.define(version: 20130830162648) do
+
+  create_table "fields", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questions", force: true do |t|
     t.string   "statement"
